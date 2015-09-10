382310d700f15637b7bc6f6947926e8c326c4f0c
ksugawara61 <katsuo.com61@gmail.com>
2015-09-10 22:49:27 +0900

    delete lockfile

--- views/bbs_view.php
+++ views/bbs_view.php
@@ -1,11 +1,39 @@
-// 投稿された内容を取得するSQLを作成して結果を取得
-$sql = "select * from `post` order by `create_at` desc";
-$result = mysql_query($sql, $link);
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"html://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+  <title>ひとこと掲示板</title>
+</head>
+<body>
+  <h1>ひとこと掲示板</h1>
 
-// 取得した結果を$postsに格納
-$posts = array();
-if ($result !== false && mysql_fetch_assoc($result)) {
-  while ($post = mysql_fetch_assoc($result)) {
-    $posts[] = $post;
-  }
-}
+  <form action="bbs.php" method="post">
+    <?php if (count($errors)): ?>
+    <ul class="error_list">
+      <?php foreach ($errors as $error): ?>
+      <li>
+        <?php echo htmlspecialchars($error, ENT_QUOTES, 'UTF-8') ?>
+      </li>
+      <?php endforeach ?>
+    </ul>
+    <?php endif; ?>
+
+    名前: <input type="text" name="name" /><br />
+    ひとこと: <input type="text" name="comment" size="60" /><br />
+    <input type="submit" name="submit" value="送信" />
+  </form>
+
+  <?php if (count($posts) > 0): ?>
+  <ul>
+    <?php foreach ($posts as $post): ?>
+    <li>
+      <?php echo htmlspecialchars($post['name'], ENT_QUOTES, 'UTF-8'); ?>
+      <?php echo htmlspecialchars($post['comment'], ENT_QUOTES, 'UTF-8'); ?>
+      - <?php echo htmlspecialchars($post['create_at'], ENT_QUOTES, 'UTF-8'); ?>
+    </li>
+    <?php endforeach; ?>
+  </ul>
+  <?php endif; ?>
+
+</body>
+</html>
